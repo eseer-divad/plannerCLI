b8b221dd4c071adad170b5c5feb1c43002e0c3ec
David Reese <dcr54@uakron.edu>
2024-02-24 22:14:22 -0500

    Document main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use clap::{command, ArgMatches, Arg};
 
 fn main() {
+    // Use the `command!()` macro and `Arg::new` with the argument name as a str literal
+    // chains the clap methods & arguments together
     let match_result: ArgMatches = command!()
         .arg(Arg::new("firstname"))
         .arg(Arg::new("lastname"))
         .get_matches();
 
+    // utilize the arg matches by printing them
     if let Some(firstname) = match_result.value_of("firstname") {
         println!("First name: {}", firstname);
     }
