110db03667380fc03f0c6dccbb35558a171c60d7
David Reese <dcr54@uakron.edu>
2024-02-24 22:11:22 -0500

    Add first test command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use clap::{command, Arg};
+use clap::{command, ArgMatches, Arg};
 
 fn main() {
-    println!("Hello, plannerCLI user!");
-    command!().arg(
-        Arg::new("firstname")
-    );
+    let match_result: ArgMatches = command!()
+        .arg(Arg::new("firstname"))
+        .arg(Arg::new("lastname"))
+        .get_matches();
+
+    if let Some(firstname) = match_result.value_of("firstname") {
+        println!("First name: {}", firstname);
+    }
+    if let Some(lastname) = match_result.value_of("lastname") {
+        println!("Last name: {}", lastname);
+    }
 }
